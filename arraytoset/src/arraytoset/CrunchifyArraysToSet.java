5cce9f7a18037818183a87cdfabcaa215d3b547a
renuka <renutarse1@gmail.com>
2017-09-14 15:34:38 +0530

    arraytoset commit

---
+++ arraytoset/src/arraytoset/CrunchifyArraysToSet.java
@@ -0,0 +1,31 @@
+package arraytoset;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+ 
+
+
+public class CrunchifyArraysToSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Integer[] numbers = { 7, 7, 8, 9, 10, 8, 8, 9, 6, 5, 4 };
+		  
+		    Set<Integer> set = new HashSet<Integer>(Arrays.asList(numbers));
+		 
+		    System.out.println("Print Set Value via toString(): " + set.toString());
+		 
+		    //if you want to use Iterator to print value
+		    System.out.println("\nPrint Set Value via Iterator: ");
+		 
+		    for (Iterator<Integer> iterator = set.iterator(); iterator.hasNext();) {
+		      Object o = iterator.next();
+		      System.out.print(o + " ");
+		    }
+		}
+
+
+	}
+
+
